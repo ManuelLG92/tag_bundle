67f831bd71458e1060336ab7d569c7041ca02d05
ManuelLG92 <lavacars.ecologic@gmail.com>
2021-12-30 09:26:22 +0100

    Fix: convert config folder name to  StudlyCaps convention.

--- src/DependencyInjection/TaggingExtension.php
+++ src/DependencyInjection/TaggingExtension.php
@@ -16,7 +16,7 @@ class TaggingExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/Config'));
         $loader->load('services.yaml');
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
