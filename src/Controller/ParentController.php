82b414dca20899f852528f866defc61adf4f673a
ManuelLG92 <lavacars.ecologic@gmail.com>
2021-12-31 09:41:57 +0100

    Fix: remove extends AbstractController.

--- src/Controller/ParentController.php
+++ src/Controller/ParentController.php
@@ -5,7 +5,7 @@ namespace Wamb\TaggingBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class ParentController extends AbstractController
+class ParentController
 {
     public function __construct(protected ContainerInterface $container )
     {
