cbe8957e96535a69428737bc411ed16945045a47
ManuelLG92 <lavacars.ecologic@gmail.com>
2021-12-31 10:58:57 +0100

    Fix: Change xml to yaml.

--- src/DependencyInjection/TaggingExtension.php
+++ src/DependencyInjection/TaggingExtension.php
@@ -19,8 +19,8 @@ class TaggingExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
        //$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-       $loader = new XmlFileLoader($container, new FileLocator('@WambTaggingBundle/Resources/config'));
-       $loader->load('services.xml');
+       $loader = new YamlFileLoader($container, new FileLocator('@WambTaggingBundle/Resources/config'));
+       $loader->load('services.yml');
         /*$configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         foreach ($config as $key => $value) {
